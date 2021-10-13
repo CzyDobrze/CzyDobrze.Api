05a8f112411bc1bee977c060a8f40061e34af9b4
Blazej Sak <blazej.sak@icloud.com>
2021-10-14 00:25:17 +0200

    Add core objects

---
+++ src/Core/Entity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CzyDobrze.Core
+{
+    public abstract class Entity
+    {
+        protected Entity()
+        {
+            //For EF
+        }
+
+        protected Entity(Guid id, DateTime created, DateTime updated)
+        {
+            Id = id;
+            Created = created;
+            Updated = updated;
+        }
+        
+        public Guid Id { get; }
+        public DateTime Created { get; }
+        public DateTime Updated { get; private set; }
+
+        public void Update(DateTime updated)
+        {
+            Updated = updated;
+        }
+    }
+}
---
+++ src/Core/ValueObject.cs
@@ -0,0 +1,52 @@
+﻿#nullable enable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CzyDobrze.Core
+{
+    public abstract class ValueObject
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>Objects used to compare value object of the same type.</returns>
+        protected abstract IEnumerable<object?> GetEqualityComponents();
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null)
+            {
+                return false;
+            }
+
+            if (obj.GetType() != GetType())
+            {
+                return false;
+            }
+
+            return ((ValueObject)obj)
+                .GetEqualityComponents()
+                .SequenceEqual(GetEqualityComponents());
+        }
+
+        public override int GetHashCode()
+        {
+            return GetEqualityComponents()
+                .Where(x=>x is not null)
+                .Select(x => x!.GetHashCode())
+                .Aggregate(HashCode.Combine);
+        }
+        
+        
+        public static bool operator==(ValueObject a, object? b)
+        {
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(ValueObject a, object? b)
+        {
+            return !(a == b);
+        }
+    }
+}
