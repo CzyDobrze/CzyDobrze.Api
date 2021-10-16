4428d7911f0905d9c0913a46561fac8ed68a2479
Blazej Sak <blazej.sak@icloud.com>
2021-10-16 20:18:06 +0200

    Project improvements (#25)
    * Disable automatic browser launch.
    
    * Move Swagger configuration outside of Startup.cs to SwaggerUtils.cs; Add CORS policy; Enable Swagger regardless of app environment (also in Production).
    
    * Add Application and Infrastructure dependency injection

---
+++ src/Application/Di.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CzyDobrze.Application
+{
+    public static class Di
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
---
+++ src/Infrastructure/Di.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CzyDobrze.Infrastructure
+{
+    public static class Di
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
