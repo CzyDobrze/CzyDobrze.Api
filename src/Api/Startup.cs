a1c6207fe1da3d3e0dea8826c757f2d794b89970
Blazej Sak <blazej.sak@icloud.com>
2021-10-16 20:06:58 +0200

    Move Swagger configuration outside of Startup.cs to SwaggerUtils.cs; Add CORS policy; Enable Swagger regardless of app environment (also in Production).

--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -1,16 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using CzyDobrze.Api.Utils;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 
 namespace CzyDobrze.Api
 {
@@ -25,12 +18,9 @@ namespace CzyDobrze.Api
 
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CzyDobrze.Api", Version = "v1" });
-            });
+            
+            services.AddSwagger();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -38,12 +28,13 @@ namespace CzyDobrze.Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CzyDobrze.Api v1"));
             }
 
-            app.UseRouting();
+            app.UseConfiguredSwagger();
 
+            app.UseRouting();
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+            
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
