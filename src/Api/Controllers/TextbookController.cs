01917b581e6714d23f14e641a1a55ca4709d6078
Blazej Sak <blazej.sak@icloud.com>
2021-10-16 18:54:13 +0200

    Add Controller

---
+++ src/Api/Controllers/TextbookController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using CzyDobrze.Application.Textbooks.Command.CreateTextbook;
+using CzyDobrze.Application.Textbooks.Queries.GetTextbookById;
+using CzyDobrze.Domain.Content.Textbook;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CzyDobrze.Api.Controllers
+{
+    [ApiController]
+    [Route("textbook")]
+    public class TextbookController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public TextbookController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<Textbook> GetTextbookById(Guid id)
+        {
+            return await _mediator.Send(new GetTextbookById(id));
+        }
+
+        [HttpPost]
+        public async Task<Textbook> CreateTextbook(CreateTextbook model)
+        {
+            return await _mediator.Send(model);
+        }
+    }
+}
