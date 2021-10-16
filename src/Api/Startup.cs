901bce7508dbcd7473d634e22c9d5e45a7913552
Blazej Sak <blazej.sak@icloud.com>
2021-10-16 19:58:08 +0200

    Add dummy (writing to singleton List) textbook repository which shows how it should work (uses Reflection to modify Core Entity fields - Id and Created). We don't need this but may be useful as a showcase or for Unit Tests.

--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -1,17 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using CzyDobrze.Api.Filters;
 using CzyDobrze.Application;
+using CzyDobrze.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace CzyDobrze.Api
@@ -30,6 +24,7 @@ namespace CzyDobrze.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddApplication();
+            services.AddInfrastructure();
             
             services.AddControllers(options =>
             {
---
+++ src/Infrastructure/Di.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CzyDobrze.Infrastructure
+{
+    public static class Di
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddDummyTextbookRepository();
+
+            return services;
+        }
+    }
+}
