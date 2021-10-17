187b5772533b691e4fb860026bf8cc20c3fe4bd7
Krzysztof Nasuta <krzysiek.com.pl@wp.pl>
2021-10-17 22:13:44 +0200

    Add SectionController

---
+++ src/Api/Controllers/SectionController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CzyDobrze.Api.Controllers
+{
+    public class SectionController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
