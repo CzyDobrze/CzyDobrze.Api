4e55805fc259ba10fa812b37aa0dc6224ee41f47
Krzysztof Nasuta <krzysiek.com.pl@wp.pl>
2021-10-21 17:47:40 +0200

    Add create answer endpoint

---
+++ src/Api/Controllers/AnswerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using CzyDobrze.Application.Answers.Commands.CreateAnswer;
+using CzyDobrze.Domain.Content.Answer;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CzyDobrze.Api.Controllers
+{
+    [ApiController]
+    [Route("/api/answer")]
+    public class AnswerController : Controller
+    {
+        private readonly IMediator _mediator;
+        
+        public AnswerController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(Answer))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> CreateAnswer(CreateAnswer model)
+        {
+            var answer = await _mediator.Send(model);
+            
+            return Created($"/api/answer/{answer.Id}", answer);
+        }
+    }
+}
---
+++ src/Application/Answers/Commands/CreateAnswer/CreateAnswerValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace CzyDobrze.Application.Answers.Commands.CreateAnswer
+{
+    public class CreateAnswerValidator : AbstractValidator<CreateAnswer>
+    {
+        public CreateAnswerValidator()
+        {
+            RuleFor(x => x.Content)
+                .NotEmpty();
+
+            RuleFor(x => x.ExerciseId)
+                .NotEmpty();
+        }
+    }
+}
