9bdff9006c970932c5014dc0da14acd73f85e44d
Blazej Sak <blazej.sak@icloud.com>
2021-10-21 22:18:06 +0200

    Add Dependency Injection

--- src/Infrastructure/Persistence/Di.cs
+++ src/Infrastructure/Persistence/Di.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using CzyDobrze.Application.Common.Interfaces.Persistence.Content;
+using CzyDobrze.Infrastructure.Persistence.Implementations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace CzyDobrze.Infrastructure.Persistence
 {
@@ -6,6 +9,19 @@ namespace CzyDobrze.Infrastructure.Persistence
     {
         public static IServiceCollection AddPersistence(this IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlite("Data Source=czydobrze.db", x =>
+                {
+                    x.MigrationsAssembly(typeof(AppDbContext).Assembly.GetName().Name);
+                });
+            });
+
+            services.AddTransient<ITextbookRepository, TextbookRepository>();
+            services.AddTransient<ISectionRepository, SectionRepository>();
+            services.AddTransient<IExerciseRepository, ExerciseRepository>();
+            services.AddTransient<IAnswerRepository, AnswerRepository>();
+            
             return services;
         }
     }
